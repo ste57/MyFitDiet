641f0a0e3dd3fb09c9bbe5255f7f6ef6f1a9edff
Stephen Sowole <tunsow@hotmail.com>
2015-03-13 06:47:53 +0000

    First Commit
    - Added Xcode Project

---
+++ My Fit Diet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My Fit Diet
+//
+//  Created by Stephen Sowole on 06/03/2015.
+//  Copyright (c) 2015 Stephen Sowole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My Fit Diet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My Fit Diet
+//
+//  Created by Stephen Sowole on 06/03/2015.
+//  Copyright (c) 2015 Stephen Sowole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
