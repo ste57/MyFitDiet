4a8f2d5b08bff98efe51555dcf6bb3263e041bb3
Stephen Sowole <tunsow@hotmail.com>
2015-03-30 03:25:05 +0100

    Added Login View Controller
    - Added LoginViewController File
    - Added Plain Launch Images For All Devices

---
+++ My Fit Diet/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  My Fit Diet
+//
+//  Created by Stephen Sowole on 30/03/2015.
+//  Copyright (c) 2015 Stephen Sowole. All rights reserved.
+//
+
+// Displayed Images
+
+// NSUserDefaults
+
+// NSNotifications
---
+++ My Fit Diet/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  My Fit Diet
+//
+//  Created by Stephen Sowole on 30/03/2015.
+//  Copyright (c) 2015 Stephen Sowole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
